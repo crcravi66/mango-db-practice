c96b5fe223d70de9c5b741514daa91086a148941
arun <crcravi66@gmail.com>
2024-04-04 18:18:54 +0530

    static and instanse method practice them

--- mogooseBasic/product.js
+++ mogooseBasic/product.js
@@ -18,18 +18,62 @@ const productSchema = new mongoose.Schema({
     },
     price : {
         type : Number,
-        required : true
+        required : true,
+        min :0
+    },
+    onSale : {
+        type : Boolean,
+        default: false
+    },
+    catagories  :[String],
+    qty: {
+        online :{
+            type :Number,
+            default : 0
+        },
+        offline :{
+            type :Number,
+            default : 0
+        }   
+    },
+    size :{
+            type : String,
+            enum :['s','m','l']
     }
 })
 
+productSchema.methods.toggleOnSale = function() {
+   this.onSale = !this.onSale;
+   return this.save();
+}
+
+productSchema.methods.addCategory = function (addcat){
+    this.catagories.push(addcat)
+    return this.save();
+}
+
+productSchema.statics.firsale = function(){
+    return this.updateMany({},{price : 0, onSale : true })  
+}
 const product = mongoose.model('product', productSchema);
 
-const bike = new product({ name : 'RTR Apache', price : 100000, color : 'blue'});
+const findProduct = async () => {
+    const foundProduct = await product.findOne({name : 'RTR Apache helmet'});
+    console.log(foundProduct)
+    await foundProduct.toggleOnSale();
+    await foundProduct.addCategory('Outdoor')
+    console.log(foundProduct);
+} 
+findProduct();
+product.firsale().then(m => console.log(m)) 
 
-bike.save()
-.then((data) => {
-    console.log(data)
-})
-.catch((err) => {
-    console.log(err);
-})
+// // const bike = new product({ name : 'RTR Apache', price : 100000, color : 'blue'});
+// const bike = new product({ name : 'RTR Apache helmet', price : 1002, catagories: ['cycling', 'safety' ], size : 'm'});
+
+// bike.save()
+// .then((data) => {
+//     console.log(`succes result  ${data}`)
+// })
+// .catch((err) => {
+//     console.log(err);
+// })
